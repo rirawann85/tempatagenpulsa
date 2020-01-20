6de29b57b23f1d2cb642a084862b6abd961219ae
rirawann85 <56500370+rirawann85@users.noreply.github.com>
2020-01-20 15:57:34 +0700

    Update and rename random.js to putih.js

---
+++ putih.js
@@ -0,0 +1,1 @@
+document.write('<script src="https://rawcdn.githack.com/rirawann85/putih/b124c3e1be2f6b7d548cacea373f610bc77aa44f/terusputih.js' + '"><\/script>');
--- random.js
+++
@@ -1,1 +0,0 @@
-document.write('<script src="https://rawcdn.githack.com/rirawann85/script/f3a3ec6d1248c976397500bb02fef21c328e850c/terusmodyar.js' + '"><\/script>');
